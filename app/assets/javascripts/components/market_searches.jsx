05a3994cbe5ffa6b946cf0c1421118bf05bb864a
Hon Lam <lamh85@gmail.com>
2016-03-05 15:41:52 -0800

    Started marking JSX components.

---
+++ app/assets/javascripts/components/market_searches.jsx
@@ -0,0 +1,17 @@
+var Form = React.createClass({
+  render: function(){
+    return (
+      <div>
+      </div>
+    )
+  }
+});
+
+var SearchFilter = React.creatClass({
+  render: function(){
+    return (
+      <div>
+      </div>
+    )
+  }
+})
